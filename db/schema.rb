8c148b512ece46dd08821c4d6ed8af3b8883bccc
Ignacio Borrell <ignacioborrellr@gmail.com>
2023-12-04 22:28:21 +0100

    nose

--- db/schema.rb
+++ db/schema.rb
@@ -80,15 +80,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_094053) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
-  create_table "flats", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.float "latitude"
-    t.float "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "messages", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "chat_id", null: false
